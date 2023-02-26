4c01c6646f82e7f796a31a3264eba92d34e4d07f
Anushka Verma <anushkaverma779@gmail.com>
2023-02-26 20:52:11 +0530

    done

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+//your code here
+let input = document.getElementById("evaluatedText");
+let h3 = document.getElementById("wordCount");
+
+let count = 0;
+let keycount = 0;
+let spacecount = 0;
+
+input.addEventListener("keyup", function(e) {
+
+	let txt = input.value.trim();
+  h3.textContent = txt.split(/\s+/).filter((item) => item).length;
+  /*   while(e.key==" ") {
+      continue;
+    } */
+    
+})
+// h3.textContent = count + 1;
